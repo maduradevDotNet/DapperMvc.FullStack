26412a0d19598d145e6986cde06e9a2b499e36d9
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-08-11 21:58:03 +0530

    database config

---
+++ DapperMvcDemo.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperMvcDemo.Data.Data
+{
+    internal class ApplicationDbContext
+    {
+        private readonly IConfiguration _config;
+
+        public ApplicationDbContext(IConfiguration config)
+        {
+            _config=config;
+        }
+
+        public async Task<IEnumerable<T>> getData<T, P>(string spName,P parameters,string connectionId= "DefaultConnection")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+            return await connection.QueryAsync<T>(spName,commandType:CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>(string spName,T parameters, string connectionId = "DefaultConnection")
+        {
+            using IDbConnection connection=new SqlConnection(_config.GetConnectionString(_config.GetConnectionString(connectionId)));
+            await connection.ExecuteAsync(spName, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+    }
+}
