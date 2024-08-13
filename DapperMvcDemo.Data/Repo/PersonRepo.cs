9183b94f98b754b62da96ee5b088bb1efdf041a7
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-08-13 11:13:43 +0530

    update Store Procedure Crud

--- DapperMvcDemo.Data/Repo/PersonRepo.cs
+++ DapperMvcDemo.Data/Repo/PersonRepo.cs
@@ -1,13 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using DapperMvcDemo.Data.Data;
+using DapperMvcDemo.Data.Models.Domain;
 
 namespace DapperMvcDemo.Data.Repo
 {
     public class PersonRepo: IPersonRepo
     {
+        private readonly IApplicationDbContext _db;
+
+        public PersonRepo(IApplicationDbContext db)
+        {
+            _db=db;
+        }
+
+        public async Task<bool> AddAsync(Person person)
+        {
+            try
+            {
+                await _db.SaveData("sp_create_person", new { person.Name, person.Email, person.Address });
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+
+            }
+        }
+
+
+        public async Task<bool> UpdateAsync(Person person)
+        {
+            try
+            {
+                await _db.SaveData("sp_update_person", person);
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                return false;
+
+            }
+        }
+
 
     }
 }
